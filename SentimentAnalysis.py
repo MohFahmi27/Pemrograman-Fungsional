730a9926e3f918636415105070391e80d621d1d5
MohFahmi27 <mohammadfahmi417@gmail.com>
2021-04-03 09:30:58 +0800

    Update: Make SentimentAnalysis.py run able.

--- SentimentAnalysis.py
+++ SentimentAnalysis.py
@@ -94,12 +94,12 @@ def sentimentWordCloud(fileName:str) -> plt:
 if __name__ == "__main__":
     # nama file untuk hasil sentiment analysis    
     time1 = time.perf_counter()
-    sentimentCSV("covid")
+    sentimentCSV(fileName="covid-2021")
     time2 = time.perf_counter()
     print(f"waktu : {time2-time1}")
     print(findWeightSentiment.cache_info())
     print(findWeightInf.cache_info())
 
     # grafik untuk distribusi sentiment
-    # sentimentPlotSingleFile("covid-2021")
-    # sentimentWordCloud("covid-2021")
+    sentimentPlotSingleFile("covid-2021")
+    sentimentWordCloud("covid-2021")
